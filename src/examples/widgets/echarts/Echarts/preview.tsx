73534f502cbd3ae052a008553ebf2be4799314e0
hujianping <819502953@qq.com>
2022-03-06 23:03:59 +0800

    commit

--- src/examples/widgets/echarts/Echarts/preview.tsx
+++ src/examples/widgets/echarts/Echarts/preview.tsx
@@ -3,6 +3,7 @@ import { IWidgetProps } from '@/datav/react/interface';
 import * as echarts from 'echarts';
 import { useDebounceEffect, useSize } from 'ahooks';
 import { message } from 'antd';
+import { colorsOpt } from '@/examples/schema/echarts/colorsSchema';
 
 /** Echarts 图表通用组件，接收所有配置文件，组件只负责渲染 */
 const Echarts: React.FC<IWidgetProps> = ({ options = {}, data = null, events }) => {
@@ -29,7 +30,7 @@ const Echarts: React.FC<IWidgetProps> = ({ options = {}, data = null, events }) 
 
   useLayoutEffect(() => {
     const { options: opt } = options;
-    let echartOpt = {};
+    let echartOpt: any = {};
     try {
       if (!data || JSON.stringify(data) === '{}') return;
       const fun = `if (!data) { return data; }  return fun(data);  function fun(data){  ${opt}   }`;
@@ -39,6 +40,10 @@ const Echarts: React.FC<IWidgetProps> = ({ options = {}, data = null, events }) 
       message.error('函数执行错误');
     }
     try {
+      if (echartOpt && !echartOpt.color) {
+        const colors = colorsOpt[0];
+        echartOpt.color = colors?.color;
+      }
       myChart.current.setOption(echartOpt, true);
     } catch (error) {}
   }, [options, data]);
