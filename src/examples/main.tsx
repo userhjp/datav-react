dc6e819347d8b7e4180aff96870155b3c3a364eb
hujianping <819502953@qq.com>
2022-03-28 21:19:38 +0800

    commit

--- src/examples/main.tsx
+++ src/examples/main.tsx
@@ -5,13 +5,20 @@ import { resourceData } from './shared/resourceData';
 import * as components from './widgets';
 import { message } from 'antd';
 import { waitTime } from '@/datav/shared';
+import axios from 'axios';
 
 const SnapshotKey = 'DataV-Snapshot';
 
 async function getSnapshot() {
   await waitTime(500);
   try {
-    return JSON.parse(localStorage.getItem(SnapshotKey));
+    const json = JSON.parse(localStorage.getItem(SnapshotKey));
+    if (json) {
+      return json;
+    } else {
+      const json2 = await axios.get('/json/demo.json');
+      return json2.data;
+    }
   } catch (error) {
     localStorage.removeItem(SnapshotKey);
   }
